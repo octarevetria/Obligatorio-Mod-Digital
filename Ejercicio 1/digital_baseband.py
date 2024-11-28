306f0e0539dff8d52772b721afc14561982e46c8
Octavio Revetria <octaviorevetria@hotmail.com>
2024-11-28 19:10:30 -0300

    Cambio frecuencia de la cuadrada en p5

--- Ejercicio 1/digital_baseband.py
+++ Ejercicio 1/digital_baseband.py
@@ -65,6 +65,8 @@ class digital_baseband(gr.top_block, Qt.QWidget):
         ##################################################
         self.sps = sps = 16
         self.samp_rate = samp_rate = 32000
+        self.symbol_rate = symbol_rate = samp_rate / sps
+        self.offset = offset = 0
         self.noise = noise = 0
         self.bandwidth = bandwidth = 2000
 
@@ -72,6 +74,17 @@ class digital_baseband(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
 
+        if "int" == "int":
+        	isFloat = False
+        	scaleFactor = 1
+        else:
+        	isFloat = True
+        	scaleFactor = 1
+
+        _offset_dial_control = qtgui.GrDialControl('Offset', self, 0,100,0,"default",self.set_offset,isFloat, scaleFactor, 0, True, "'Offset'")
+        self.offset = _offset_dial_control
+
+        self.top_layout.addWidget(_offset_dial_control)
         if "real" == "int":
         	isFloat = False
         	scaleFactor = 1
@@ -109,7 +122,7 @@ class digital_baseband(gr.top_block, Qt.QWidget):
             samp_rate, #bw
             "Señal recibida", #name
             True, #plotfreq
-            False, #plotwaterfall
+            True, #plotwaterfall
             True, #plottime
             True, #plotconst
             None # parent
@@ -146,57 +159,6 @@ class digital_baseband(gr.top_block, Qt.QWidget):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 5):
             self.top_grid_layout.setColumnStretch(c, 1)
-        self.qtgui_eye_sink_x_0 = qtgui.eye_sink_f(
-            1024, #size
-            samp_rate, #samp_rate
-            1, #number of inputs
-            None
-        )
-        self.qtgui_eye_sink_x_0.set_update_time(0.10)
-        self.qtgui_eye_sink_x_0.set_samp_per_symbol(16)
-        self.qtgui_eye_sink_x_0.set_y_axis(-1, 1)
-
-        self.qtgui_eye_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_eye_sink_x_0.enable_tags(True)
-        self.qtgui_eye_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_eye_sink_x_0.enable_autoscale(False)
-        self.qtgui_eye_sink_x_0.enable_grid(False)
-        self.qtgui_eye_sink_x_0.enable_axis_labels(True)
-        self.qtgui_eye_sink_x_0.enable_control_panel(False)
-
-
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'blue', 'blue', 'blue', 'blue',
-            'blue', 'blue', 'blue', 'blue', 'blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_eye_sink_x_0.set_line_label(i, "Eye[Data {0}]".format(i))
-            else:
-                self.qtgui_eye_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_eye_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_eye_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_eye_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_eye_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_eye_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_eye_sink_x_0_win = sip.wrapinstance(self.qtgui_eye_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_eye_sink_x_0_win, 2, 0, 1, 5)
-        for r in range(2, 3):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 5):
-            self.top_grid_layout.setColumnStretch(c, 1)
         self.low_pass_filter_0 = filter.fir_filter_fff(
             1,
             firdes.low_pass(
@@ -207,11 +169,13 @@ class digital_baseband(gr.top_block, Qt.QWidget):
                 window.WIN_HAMMING,
                 6.76))
         self.digital_map_bb_0 = digital.map_bb((-1,1))
-        self.blocks_vector_insert_x_0 = blocks.vector_insert_f(numpy.zeros(sps//2), sps, 0)
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, (sps//2))
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, sps)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
         self.blocks_add_xx_0 = blocks.add_vff(1)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff((-1))
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, symbol_rate, 2, 0, offset)
         self.analog_random_source_x_1 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 128))), True)
         self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, noise, 0)
 
@@ -221,13 +185,14 @@ class digital_baseband(gr.top_block, Qt.QWidget):
         ##################################################
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.analog_random_source_x_1, 0), (self.digital_map_bb_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_eye_sink_x_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_sink_x_0_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.blocks_repeat_0, 0))
-        self.connect((self.blocks_repeat_0, 0), (self.blocks_vector_insert_x_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.qtgui_sink_x_0, 0))
-        self.connect((self.blocks_vector_insert_x_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.digital_map_bb_0, 0), (self.blocks_char_to_float_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.blocks_add_xx_0, 0))
 
@@ -245,18 +210,34 @@ class digital_baseband(gr.top_block, Qt.QWidget):
 
     def set_sps(self, sps):
         self.sps = sps
-        self.blocks_repeat_0.set_interpolation((self.sps//2))
+        self.set_symbol_rate(self.samp_rate / self.sps)
+        self.blocks_repeat_0.set_interpolation(self.sps)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.set_symbol_rate(self.samp_rate / self.sps)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.bandwidth, (self.bandwidth/10), window.WIN_HAMMING, 6.76))
-        self.qtgui_eye_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_sink_x_0_0.set_frequency_range(0, self.samp_rate)
+
+    def get_symbol_rate(self):
+        return self.symbol_rate
+
+    def set_symbol_rate(self, symbol_rate):
+        self.symbol_rate = symbol_rate
+        self.analog_sig_source_x_0.set_frequency(self.symbol_rate)
+
+    def get_offset(self):
+        return self.offset
+
+    def set_offset(self, offset):
+        self.offset = offset
+        self.analog_sig_source_x_0.set_phase(self.offset)
 
     def get_noise(self):
         return self.noise
